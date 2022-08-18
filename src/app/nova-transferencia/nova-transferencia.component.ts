5f2c8093fd803fe17b8d2f9ddf3cef4cbda74462
GuiCabral74 <guilhermerocabral@hotmail.com>
2022-08-18 14:11:34 -0300

    Seventh unit: Added route in aplication.

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,34 +1,35 @@
-import { Component, EventEmitter, Output } from "@angular/core";
-import { TransferenciaService } from "../services/transferencia.service";
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { TransferenciaService } from '../services/transferencia.service';
 
 @Component({
-    selector: 'app-nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls:['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent{
-
+export class NovaTransferenciaComponent {
   @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number;
 
-  constructor(private service: TransferenciaService){}
+  constructor(private service: TransferenciaService, private router: Router) {}
 
-  transferir(){
-    console.log("solicitada nova transferência");
+  transferir() {
+    console.log('solicitada nova transferência');
 
-    const valorEmitir = {valor: this.valor, destino: this.destino}
+    const valorEmitir = { valor: this.valor, destino: this.destino };
 
-    this.service.adicionar(valorEmitir)
-    .subscribe(resultado => {
-      console.log(resultado);
-      this.limparCampos();
-    },
-    error => console.error(error))
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.router.navigateByUrl('extrato');
+      },
+      (error) => console.error(error)
+    );
   }
 
-  limparCampos(){
+  limparCampos() {
     this.valor = 0;
     this.destino = 0;
   }
